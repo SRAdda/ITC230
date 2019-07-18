8b898daacd8f147fc008010800ef84a1ba5e95ef
Sally Ross <46608728+SRAdda@users.noreply.github.com>
2019-07-18 08:22:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const http = require("http"); 
-http.createServer((req,res) => {
-  const path = req.url.toLowerCase();
+var http = require("http"), fs = require('fs');
+
+function serveStatic(res, path, contentType, responseCode){
+  if(!responseCode) responseCode = 200;
+  console.log(__dirname + path)
+  fs.readFile(__dirname + path, function(err, data){
+      if(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+      }
+      else{
+        res.writeHead(responseCode, {'Content-Type': contentType});
+        res.end(data);
+      }
+  });
+}
+
+http.createServer(function(req,res){
+  console.log('createServer got request')
+  var path = req.url.toLowerCase();
   switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
+    case '/': 
+      serveStatic(res, '/../public/home.html', 'text/html');
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
+      res.end('About');
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
+      res.end('404:Page not found.');
+  }
+  
 }).listen(process.env.PORT || 3000);
+console.log('after createServer')
