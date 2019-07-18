899a0fd348ba31a3aacebcdf2e78365b1451bdab
Sally Ross <46608728+SRAdda@users.noreply.github.com>
2019-07-18 08:18:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
- }).listen(8081);
- 
- // Console will print the message
- console.log('Server running at http://127.0.0.1:8081/');
+const http = require("http"); 
+http.createServer((req,res) => {
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
